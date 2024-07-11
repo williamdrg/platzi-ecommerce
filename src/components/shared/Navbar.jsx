7d1cde29164f3c318edfd38e60e6a13bad28930f
Jairo Rincon <rincon303@gmail.com>
2024-07-11 08:32:55 -0500

    Update Navbar

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import '../../Styles/Navbar.css';
+const Navbar = () => {
+  return (
+
+    <div className='Contain_Navbar'>
+      <ul className='Containt_Navbar_item'>
+      <Link to = '/HomePage'> Home</Link>
+      <Link to = '/Product'>Productos</Link>
+      <Link to = '/Cart'>Cart</Link>
+      <Link to = '/Purchases'>Purchases</Link>
+      </ul>
+    </div>
+
+  )
+}
+
+export default Navbar
