f7f580c0a94c55f1d278b1549b0a475d7dc16b07
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-07-11 13:07:15 -0500

    actualización de estilos generales

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import HomePage from './HomePage'
+import HomePage from './pages/HomePage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import ProductId from './pages/ProductId'
