8208525ecb562e3acd55791b24893c0f619d5059
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-07-10 23:21:31 -0500

    actualización de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './HomePage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ProductId from './pages/ProductId'
+import Purchases from './pages/Purchases'
 
 function App() {
 
   return (
     <div>
-      <h1>hola mundo</h1>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/product/:id' element={<ProductId/>}/>
+        <Route path='/purchases' element={<Purchases/>}/>
+      </Routes>
     </div>
   )
 }
