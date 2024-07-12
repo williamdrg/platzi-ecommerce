6533822202f85f7c4d69bd9bf5bd2b5f62b5d29d
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-07-11 22:26:04 -0500

    arreglar app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,30 @@
 
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-<<<<<<< HEAD
-=======
 import Navbar from './components/shared/Navbar'
->>>>>>> 46dc4a875f616b895fdd5ab6a65fa80f713d2bdc
 import HomePage from './pages/HomePage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import ProductId from './pages/ProductId'
 import Purchases from './pages/Purchases'
-<<<<<<< HEAD
-=======
 import Product from './pages/Product'
 import Cart from './pages/Cart'
->>>>>>> 46dc4a875f616b895fdd5ab6a65fa80f713d2bdc
 
 function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-=======
       <Navbar/>
->>>>>>> 46dc4a875f616b895fdd5ab6a65fa80f713d2bdc
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
-<<<<<<< HEAD
-=======
         <Route path='/Productos' element={<Product/>}/>
         <Route path='/Cart' element={<Cart/>}/>
->>>>>>> 46dc4a875f616b895fdd5ab6a65fa80f713d2bdc
         <Route path='/product/:id' element={<ProductId/>}/>
         <Route path='/purchases' element={<Purchases/>}/>
       </Routes>
     </div>
-
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,7 @@ import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-<<<<<<< HEAD
       <App />
-=======
-          <App />
->>>>>>> 46dc4a875f616b895fdd5ab6a65fa80f713d2bdc
     </HashRouter>
   </React.StrictMode>,
 )
