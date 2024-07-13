36a30bce704847f6769b289d5302d8e1cb45f5d9
Fernando <ing.fernandovg@gmail.com>
2024-07-12 20:01:17 -0600

    advance prod_info logic _02

--- src/pages/ProductId.jsx
+++ src/pages/ProductId.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
 import './styles/productid.css'
 
 const ProductId = () => {
+
+  const [counter, setCounter] = useState(1)
 
   const {id} = useParams()
 
@@ -22,6 +24,26 @@ const ProductId = () => {
         <figure>
           <img src={product?.image} alt={product?.title} className='productid__img' />
         </figure>
+        <span>{product?.category}</span>
+      </div>
+      <div>
+        <h1>{product?.title}</h1>
+        <p>{product?.description}</p>
+      </div>
+      <div>
+        <span>Price</span>
+        <span>{product?.price}</span>
+      </div>
+      <div>
+        <span>Quantity</span>
+        <button>-</button>
+        <span>
+          {counter}
+        </span>
+        <button>+</button>
+      </div>
+      <div>
+        {/* Addition button buy */}
       </div>
     </div>
   )
