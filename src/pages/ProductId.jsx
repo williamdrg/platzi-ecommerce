6ca351854d10ca04a99fb2564beb71dcb431474b
Fernando <ing.fernandovg@gmail.com>
2024-07-12 19:49:41 -0600

    productid_begin_launch_info

--- src/pages/ProductId.jsx
+++ src/pages/ProductId.jsx
@@ -1,8 +1,29 @@
-
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+import './styles/productid.css'
 
 const ProductId = () => {
+
+  const {id} = useParams()
+
+  const [ product, getProduct ] = useFetch();
+
+  useEffect(() => {
+    const path = `/products/${id}`;
+    getProduct(path)
+  }, []);
+
+  console.log(product)
+
   return (
-    <div>ProductId</div>
+    <div className='productid'>
+      <div>
+        <figure>
+          <img src={product?.image} alt={product?.title} className='productid__img' />
+        </figure>
+      </div>
+    </div>
   )
 }
 
