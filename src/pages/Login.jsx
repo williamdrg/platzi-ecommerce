8227d85a27a337b3dfa30155805679af9f626cdd
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-07-12 21:28:12 -0500

    desarrollo del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,35 @@
+import { useForm } from "react-hook-form"
+import './styles/login.css'
+import { Link } from "react-router-dom"
 
 const Login = () => {
+  const {register, handleSubmit, reset } = useForm()
+
+  const submit = (data) => {
+      
+
+    reset({
+      username: '',
+      password: ''
+    })
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit(submit)}>
+      <div className='login__container'>
+          <h2>Name</h2>
+          <div className='login__item'>
+            <label htmlFor="username">Username</label>
+            <input {...register('username')} id='username' type="text" />
+          </div>
+          <div className='login__item'>
+            <label htmlFor="password">Password</label>
+            <input {...register('password')} id='password' type="text" />
+          </div>
+        </div>
+        <button>Submit</button>
+        <p>Don´t have an account?<Link to={'/register'}><span> Sign up</span></Link> </p>
+    </form>
   )
 }
 
