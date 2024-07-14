283511f57a8f871d991a2f21a2a72c460d56efef
Jh4stYn <jhastyn57@gmail.com>
2024-07-13 19:22:11 -0500

    Se crea 'products.slice' para obtener los productos

---
+++ src/store/slices/products.slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import urlBase from "../../utils/urlBase";
+
+const products = createSlice({
+    name: 'products',
+    initialState: null,
+    reducers: {
+        setProducts: (_state, {payload}) => payload
+    }
+})
+
+export const { setProducts } = products.actions
+
+export default products.reducer
+
+export const getProductsThunk = () => (dispatch) => {
+    const url = `${urlBase}/products`
+    axios.get(url)
+        .then(res => dispatch(setProducts(res.data)))
+        .catch(err => console.log(err))
+}
