2a529152827ce6e7faaa7ffc020d72f570610e2c
Jh4stYn <jhastyn57@gmail.com>
2024-07-13 20:20:00 -0500

    Se implementa la estructura para mostrar los productos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,30 @@
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductsThunk } from "../store/slices/products.slice";
+import ProdCard from "../components/homePage/ProdCard";
+import "./styles/homePage.css";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const products = useSelector((store) => store.products);
+  const dispatch = useDispatch();
 
-export default HomePage
+  useEffect(() => {
+    dispatch(getProductsThunk());
+  }, []);
+
+  // console.log(products)
+
+  return (
+    <div className="homepage">
+      <div className="homepage__filter"></div>
+      <div className="homepage__search"></div>
+      <div className="homepage__container">
+        {products?.map((prod) => (
+          <ProdCard key={prod.id} prod={prod} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
