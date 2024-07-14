f3d962f3c5abdfb48d3af4a1fbd0565bbd46e436
Jh4stYn <jhastyn57@gmail.com>
2024-07-13 19:24:28 -0500

    Se agrega el slice 'products'

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from './slices/auth.slice'
+import products from "./slices/products.slice";
 
 const store = configureStore({
   reducer: {
+    products,
     user: userSlice
   }
 })
