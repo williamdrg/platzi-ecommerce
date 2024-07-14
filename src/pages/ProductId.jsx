2a1bc8994a0e7d54d6eb48b709e01902e6687c47
Fernando <ing.fernandovg@gmail.com>
2024-07-14 14:17:37 -0600

    productID_fixed structure and give styles for small dispositives

--- src/pages/ProductId.jsx
+++ src/pages/ProductId.jsx
@@ -26,31 +26,44 @@ const ProductId = () => {
     return setCounter(counter + 1)
   }
 
-  // console.log(product)
+  console.log(product)
 
   return (
     <div className='productid'>
       <div className='productid__picture'>
-        <span>{product?.category}</span>
+        <span className='productid__routes'>
+          <a href="/">Home</a> 
+          {'>'}
+          {product?.title}</span>
         <figure>
           <img src={product?.image} alt={product?.title} className='productid__img' />
         </figure>
       </div>
       <div className='productid__info'>
-        <h1>{product?.title}</h1>
-        <p>{product?.description}</p>
-        <div>
-          <span>Price</span>
-          <span>{product?.price}</span>
-          <div>
-            <span>Quantity</span>
-            <button onClick={handleLess}>-</button>
-            <span>
-              {counter}
+        <h1 className='productid__title'>{product?.title}</h1>
+        <p className='productid__description'>{product?.description}</p>
+        <div className='productid__details'>
+          <div className='productid__price'>
+            <span className='productid__text-price'>Price</span>
+            <span className='productid__cost-price'>
+              ${(product?.price * counter).toFixed(2)}
             </span>
-            <button onClick={handlePlus}>+</button>
           </div>
-          <div>
+          <div className='productid__quantity'>
+            <span className='productid__text-price'>Quantity</span>
+            <div className='productid__buttons'>
+              <button onClick={handleLess} className='productid__btn'>
+                -
+              </button>
+              <span className='productid__count'>
+                {counter}
+              </span>
+              <button onClick={handlePlus} className='productid__btn'>
+                +
+              </button>
+            </div>
+          </div>
+          <div className='productid__buy-section'>
             {/* Addition button buy */}
           </div>
         </div>
